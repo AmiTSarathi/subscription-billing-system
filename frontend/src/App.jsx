8e56fae0e2bee196b53a2d49d7fc2f5e7fc63f31
Amit Gupta <126414231+AmiTSarathi@users.noreply.github.com>
2025-04-14 17:10:01 -0400

    App jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import BillView from './components/BillView'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <BillView />
+    </div>
+  )
+}
+
+export default App;
